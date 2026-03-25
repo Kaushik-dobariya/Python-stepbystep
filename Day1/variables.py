ae1babd8eaba24213e6359dc74e636455595e752
kaushik-dobariya <kumarkaushik.net@gmail.com>
2026-03-25 15:59:29 +0530

    New Precticals performed

--- Day1/variables.py
+++ Day1/variables.py
@@ -33,8 +33,22 @@ def factorial(n):
     else:
         return n * factorial(n - 1)
     
-number = int(input("Enter a number to compute its factorial: "))
+number = 6 #int(input("Enter a number to compute its factorial: "))
 result = factorial(number)
 print(result)
+
+#dict() function can be used to create a dictionary. A dictionary is a collection of key-value pairs. Each key is unique and maps to a value. The dict() function can take an iterable of key-value pairs or keyword arguments to create a dictionary.
+a=2 #int(input("Enter a number: "))
+d=dict()
+for i in range(1,a+1):
+    d[i]=i*i
+print(d)
+
+
+values=input()
+l=values.split(",")
+m=tuple(l)
+print(l)
+print(m)
 
 
